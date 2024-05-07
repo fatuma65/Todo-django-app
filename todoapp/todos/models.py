8d77d1a6775a5c3a13af9484bf87981e607ad5b1
fatuma65 <namalefatuma28@gmail.com>
2024-05-07 20:44:08 +0300

    add user authentication and views

--- todoapp/todos/models.py
+++ todoapp/todos/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class TodoItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     title = models.CharField(max_length=255)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.title
+        return f'{self.title}'
+    
